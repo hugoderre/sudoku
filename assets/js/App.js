d185c8949e397fd77483866c399d433503ec41b7
Hugo Derré <hugo.d83@outlook.fr>
2022-05-19 22:50:39 +0200

    add: actions buttons module

--- assets/js/App.js
+++ assets/js/App.js
@@ -5,14 +5,12 @@ class Sudoku {
     constructor() {
         this.rootDOM = document.getElementById( 'root' )
         this.board = new Board()
-        this.actions = new Actions()
         this.init()
     }
 
     init() {
         this.rootDOM.append( this.board.getBoard() )
-        // TODO
-        // this.rootDOM.append( this.actions.getActions() )
+        this.rootDOM.append( Actions.getActions() )
     }
 }
 
--- assets/js/Components/Actions.js
+++ assets/js/Components/Actions.js
@@ -1,9 +1,36 @@
 export default class Actions {
-    constructor() {
-
+    static getActions() {
+        const actions = document.createElement( 'div' )
+        actions.id = 'actions'
+        actions.classList.add( 'actions' )
+        actions.append( this.getNewGameButton() )
+        actions.append( this.getCheckButton() )
+        return actions
     }
 
-    getActions() {
+    static getNewGameButton() {
+        const newGameButton = document.createElement( 'button' )
+        newGameButton.id = 'newGame'
+        newGameButton.classList.add( 'action' )
+        newGameButton.innerText = 'New Game'
+        newGameButton.addEventListener( 'click', this.newGameListener.bind( this ) )
+        return newGameButton
+    }
 
+    static newGameListener( e ) {
+        console.log( 'new game' )
+    }
+
+    static getCheckButton() {
+        const checkButton = document.createElement( 'button' )
+        checkButton.id = 'check'
+        checkButton.classList.add( 'action' )
+        checkButton.innerText = 'Check'
+        checkButton.addEventListener( 'click', this.checkListener.bind( this ) )
+        return checkButton
+    }
+
+    static checkListener( e ) {
+        console.log( 'check' )
     }
 }
