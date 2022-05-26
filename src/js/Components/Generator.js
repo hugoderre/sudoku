031328a61821ef87442fc9e107f603afe2e3ad61
DESKTOP-OBASNDT\Hugo <hugo.d83@outlook.fr>
2022-05-26 14:20:07 +0200

    refactor: move some debug function in Helpers util class

--- src/js/Components/Generator.js
+++ src/js/Components/Generator.js
@@ -35,8 +35,8 @@ export default class Generator {
         this.hideSomeCellsInGroups()
 
         // Debug
-        // this.getRowsValuesDuplicateCount()
-        // this.getColumnsValuesDuplicateCount()
+        // Helpers.getRowsValuesDuplicateCount()
+        // Helpers.getColumnsValuesDuplicateCount()
         
         return this.grid
     }
@@ -130,75 +130,6 @@ export default class Generator {
                 this.board.cells[i].classList.add('static') // Base values
             }
         }
-    }
-
-    gridHasDuplicateNumberInGroups() {
-        const groups = Helpers.convertRowValuesToGroupedValues( this.grid );
-        for (let group of groups) {
-            if ((new Set(group)).size !== group.length) {
-                return true
-            }
-        }
-        return false
-    }
-
-    getRowsValuesDuplicateCount() {
-        const rows = []
-        rows[0] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[0]) + this.board.getCellValue( this.board.cells[1]) + this.board.getCellValue( this.board.cells[2]) + this.board.getCellValue( this.board.cells[9]) + this.board.getCellValue( this.board.cells[10]) + this.board.getCellValue( this.board.cells[11]) + this.board.getCellValue( this.board.cells[18]) + this.board.getCellValue( this.board.cells[19]) + this.board.getCellValue( this.board.cells[20]) )
-        rows[1] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[3]) + this.board.getCellValue( this.board.cells[4]) + this.board.getCellValue( this.board.cells[5]) + this.board.getCellValue( this.board.cells[12]) + this.board.getCellValue( this.board.cells[13]) + this.board.getCellValue( this.board.cells[14]) + this.board.getCellValue( this.board.cells[21]) + this.board.getCellValue( this.board.cells[22]) + this.board.getCellValue( this.board.cells[23]))
-        rows[2] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[6]) + this.board.getCellValue( this.board.cells[7]) + this.board.getCellValue( this.board.cells[8]) + this.board.getCellValue( this.board.cells[15]) + this.board.getCellValue( this.board.cells[16]) + this.board.getCellValue( this.board.cells[17]) + this.board.getCellValue( this.board.cells[24]) + this.board.getCellValue( this.board.cells[25]) + this.board.getCellValue( this.board.cells[26]))
-        rows[3] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[27]) + this.board.getCellValue( this.board.cells[28]) + this.board.getCellValue( this.board.cells[29]) + this.board.getCellValue( this.board.cells[36]) + this.board.getCellValue( this.board.cells[37]) + this.board.getCellValue( this.board.cells[38]) + this.board.getCellValue( this.board.cells[45]) + this.board.getCellValue( this.board.cells[46]) + this.board.getCellValue( this.board.cells[47]))
-        rows[4] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[30]) + this.board.getCellValue( this.board.cells[31]) + this.board.getCellValue( this.board.cells[32]) + this.board.getCellValue( this.board.cells[39]) + this.board.getCellValue( this.board.cells[40]) + this.board.getCellValue( this.board.cells[41]) + this.board.getCellValue( this.board.cells[48]) + this.board.getCellValue( this.board.cells[49]) + this.board.getCellValue( this.board.cells[50]))
-        rows[5] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[33]) + this.board.getCellValue( this.board.cells[34]) + this.board.getCellValue( this.board.cells[35]) + this.board.getCellValue( this.board.cells[42]) + this.board.getCellValue( this.board.cells[43]) + this.board.getCellValue( this.board.cells[44]) + this.board.getCellValue( this.board.cells[51]) + this.board.getCellValue( this.board.cells[52]) + this.board.getCellValue( this.board.cells[53]))
-        rows[6] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[54]) + this.board.getCellValue( this.board.cells[55]) + this.board.getCellValue( this.board.cells[56]) + this.board.getCellValue( this.board.cells[63]) + this.board.getCellValue( this.board.cells[64]) + this.board.getCellValue( this.board.cells[65]) + this.board.getCellValue( this.board.cells[72]) + this.board.getCellValue( this.board.cells[73]) + this.board.getCellValue( this.board.cells[74]))
-        rows[7] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[57]) + this.board.getCellValue( this.board.cells[58]) + this.board.getCellValue( this.board.cells[59]) + this.board.getCellValue( this.board.cells[66]) + this.board.getCellValue( this.board.cells[67]) + this.board.getCellValue( this.board.cells[68]) + this.board.getCellValue( this.board.cells[75]) + this.board.getCellValue( this.board.cells[76]) + this.board.getCellValue( this.board.cells[77]))
-        rows[8] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[60]) + this.board.getCellValue( this.board.cells[61]) + this.board.getCellValue( this.board.cells[62]) + this.board.getCellValue( this.board.cells[69]) + this.board.getCellValue( this.board.cells[70]) + this.board.getCellValue( this.board.cells[71]) + this.board.getCellValue( this.board.cells[78]) + this.board.getCellValue( this.board.cells[79]) + this.board.getCellValue( this.board.cells[80]))
-
-        // Debug rows cell values
-        for (let i = 0; i < rows.length; i++) {
-            document.getElementById('duplicates__row-' + (i + 1)).innerHTML = rows[i]
-        }
-
-        return rows
-    }
-
-    
-
-    getColumnsValuesDuplicateCount() {
-        const cols = []
-        cols[0] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[0]) + this.board.getCellValue( this.board.cells[3]) + this.board.getCellValue( this.board.cells[6]) + this.board.getCellValue( this.board.cells[27]) + this.board.getCellValue( this.board.cells[30]) + this.board.getCellValue( this.board.cells[33]) + this.board.getCellValue( this.board.cells[54]) + this.board.getCellValue( this.board.cells[57]) + this.board.getCellValue( this.board.cells[60]))
-        cols[1] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[1]) + this.board.getCellValue( this.board.cells[4]) + this.board.getCellValue( this.board.cells[7]) + this.board.getCellValue( this.board.cells[28]) + this.board.getCellValue( this.board.cells[31]) + this.board.getCellValue( this.board.cells[34]) + this.board.getCellValue( this.board.cells[55]) + this.board.getCellValue( this.board.cells[58]) + this.board.getCellValue( this.board.cells[61]))
-        cols[2] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[2]) + this.board.getCellValue( this.board.cells[5]) + this.board.getCellValue( this.board.cells[8]) + this.board.getCellValue( this.board.cells[29]) + this.board.getCellValue( this.board.cells[32]) + this.board.getCellValue( this.board.cells[35]) + this.board.getCellValue( this.board.cells[56]) + this.board.getCellValue( this.board.cells[59]) + this.board.getCellValue( this.board.cells[62]))
-        cols[3] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[9]) + this.board.getCellValue( this.board.cells[12]) + this.board.getCellValue( this.board.cells[15]) + this.board.getCellValue( this.board.cells[36]) + this.board.getCellValue( this.board.cells[39]) + this.board.getCellValue( this.board.cells[42]) + this.board.getCellValue( this.board.cells[63]) + this.board.getCellValue( this.board.cells[66]) + this.board.getCellValue( this.board.cells[69]))
-        cols[4] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[10]) + this.board.getCellValue( this.board.cells[13]) + this.board.getCellValue( this.board.cells[16]) + this.board.getCellValue( this.board.cells[37]) + this.board.getCellValue( this.board.cells[40]) + this.board.getCellValue( this.board.cells[43]) + this.board.getCellValue( this.board.cells[64]) + this.board.getCellValue( this.board.cells[67]) + this.board.getCellValue( this.board.cells[70]))
-        cols[5] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[11]) + this.board.getCellValue( this.board.cells[14]) + this.board.getCellValue( this.board.cells[17]) + this.board.getCellValue( this.board.cells[38]) + this.board.getCellValue( this.board.cells[41]) + this.board.getCellValue( this.board.cells[44]) + this.board.getCellValue( this.board.cells[65]) + this.board.getCellValue( this.board.cells[68]) + this.board.getCellValue( this.board.cells[71]))
-        cols[6] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[18]) + this.board.getCellValue( this.board.cells[21]) + this.board.getCellValue( this.board.cells[24]) + this.board.getCellValue( this.board.cells[45]) + this.board.getCellValue( this.board.cells[48]) + this.board.getCellValue( this.board.cells[51]) + this.board.getCellValue( this.board.cells[72]) + this.board.getCellValue( this.board.cells[75]) + this.board.getCellValue( this.board.cells[78]))
-        cols[7] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[19]) + this.board.getCellValue( this.board.cells[22]) + this.board.getCellValue( this.board.cells[25]) + this.board.getCellValue( this.board.cells[46]) + this.board.getCellValue( this.board.cells[49]) + this.board.getCellValue( this.board.cells[52]) + this.board.getCellValue( this.board.cells[73]) + this.board.getCellValue( this.board.cells[76]) + this.board.getCellValue( this.board.cells[79]))
-        cols[8] = this.getMaxDuplicateValues(this.board.getCellValue( this.board.cells[20]) + this.board.getCellValue( this.board.cells[23]) + this.board.getCellValue( this.board.cells[26]) + this.board.getCellValue( this.board.cells[47]) + this.board.getCellValue( this.board.cells[50]) + this.board.getCellValue( this.board.cells[53]) + this.board.getCellValue( this.board.cells[74]) + this.board.getCellValue( this.board.cells[77]) + this.board.getCellValue( this.board.cells[80]))
-
-        // Debug columns cell values
-        for (let i = 0; i < cols.length; i++) {
-            document.getElementById('duplicates__col-' + (i + 1)).innerHTML = cols[i]
-        }
-
-        return cols
-    }
-
-    getMaxDuplicateValues(row) {
-        let count = {}
-        for (let i = 0; i < row.length; i++) {
-            if (count[row[i]]) {
-                count[row[i]]++
-            } else {
-                count[row[i]] = 1
-            }
-        }
-        let out = []
-        for(let c of Object.values(count)) {
-            out.push(c)
-        }
-        out.sort((a, b) => b - a)
-        return out[0]
     }
 }
 
--- src/js/Components/Helpers.js
+++ src/js/Components/Helpers.js
@@ -53,4 +53,71 @@ export default class Helpers {
         result.push(subArray)
         return result
     }
+
+    static gridHasDuplicateNumberInGroups() {
+        const groups = Helpers.convertRowValuesToGroupedValues( this.grid );
+        for (let group of groups) {
+            if ((new Set(group)).size !== group.length) {
+                return true
+            }
+        }
+        return false
+    }
+
+    static getRowsValuesDuplicateCount( board ) {
+        const rows = []
+        rows[0] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[0]) + board.getCellValue( board.cells[1]) + board.getCellValue( board.cells[2]) + board.getCellValue( board.cells[9]) + board.getCellValue( board.cells[10]) + board.getCellValue( board.cells[11]) + board.getCellValue( board.cells[18]) + board.getCellValue( board.cells[19]) + board.getCellValue( board.cells[20]) )
+        rows[1] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[3]) + board.getCellValue( board.cells[4]) + board.getCellValue( board.cells[5]) + board.getCellValue( board.cells[12]) + board.getCellValue( board.cells[13]) + board.getCellValue( board.cells[14]) + board.getCellValue( board.cells[21]) + board.getCellValue( board.cells[22]) + board.getCellValue( board.cells[23]))
+        rows[2] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[6]) + board.getCellValue( board.cells[7]) + board.getCellValue( board.cells[8]) + board.getCellValue( board.cells[15]) + board.getCellValue( board.cells[16]) + board.getCellValue( board.cells[17]) + board.getCellValue( board.cells[24]) + board.getCellValue( board.cells[25]) + board.getCellValue( board.cells[26]))
+        rows[3] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[27]) + board.getCellValue( board.cells[28]) + board.getCellValue( board.cells[29]) + board.getCellValue( board.cells[36]) + board.getCellValue( board.cells[37]) + board.getCellValue( board.cells[38]) + board.getCellValue( board.cells[45]) + board.getCellValue( board.cells[46]) + board.getCellValue( board.cells[47]))
+        rows[4] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[30]) + board.getCellValue( board.cells[31]) + board.getCellValue( board.cells[32]) + board.getCellValue( board.cells[39]) + board.getCellValue( board.cells[40]) + board.getCellValue( board.cells[41]) + board.getCellValue( board.cells[48]) + board.getCellValue( board.cells[49]) + board.getCellValue( board.cells[50]))
+        rows[5] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[33]) + board.getCellValue( board.cells[34]) + board.getCellValue( board.cells[35]) + board.getCellValue( board.cells[42]) + board.getCellValue( board.cells[43]) + board.getCellValue( board.cells[44]) + board.getCellValue( board.cells[51]) + board.getCellValue( board.cells[52]) + board.getCellValue( board.cells[53]))
+        rows[6] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[54]) + board.getCellValue( board.cells[55]) + board.getCellValue( board.cells[56]) + board.getCellValue( board.cells[63]) + board.getCellValue( board.cells[64]) + board.getCellValue( board.cells[65]) + board.getCellValue( board.cells[72]) + board.getCellValue( board.cells[73]) + board.getCellValue( board.cells[74]))
+        rows[7] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[57]) + board.getCellValue( board.cells[58]) + board.getCellValue( board.cells[59]) + board.getCellValue( board.cells[66]) + board.getCellValue( board.cells[67]) + board.getCellValue( board.cells[68]) + board.getCellValue( board.cells[75]) + board.getCellValue( board.cells[76]) + board.getCellValue( board.cells[77]))
+        rows[8] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[60]) + board.getCellValue( board.cells[61]) + board.getCellValue( board.cells[62]) + board.getCellValue( board.cells[69]) + board.getCellValue( board.cells[70]) + board.getCellValue( board.cells[71]) + board.getCellValue( board.cells[78]) + board.getCellValue( board.cells[79]) + board.getCellValue( board.cells[80]))
+
+        // Debug rows cell values
+        for (let i = 0; i < rows.length; i++) {
+            document.getElementById('duplicates__row-' + (i + 1)).innerHTML = rows[i]
+        }
+
+        return rows
+    }
+
+    static getColumnsValuesDuplicateCount( board ) {
+        const cols = []
+        cols[0] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[0]) + board.getCellValue( board.cells[3]) + board.getCellValue( board.cells[6]) + board.getCellValue( board.cells[27]) + board.getCellValue( board.cells[30]) + board.getCellValue( board.cells[33]) + board.getCellValue( board.cells[54]) + board.getCellValue( board.cells[57]) + board.getCellValue( board.cells[60]))
+        cols[1] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[1]) + board.getCellValue( board.cells[4]) + board.getCellValue( board.cells[7]) + board.getCellValue( board.cells[28]) + board.getCellValue( board.cells[31]) + board.getCellValue( board.cells[34]) + board.getCellValue( board.cells[55]) + board.getCellValue( board.cells[58]) + board.getCellValue( board.cells[61]))
+        cols[2] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[2]) + board.getCellValue( board.cells[5]) + board.getCellValue( board.cells[8]) + board.getCellValue( board.cells[29]) + board.getCellValue( board.cells[32]) + board.getCellValue( board.cells[35]) + board.getCellValue( board.cells[56]) + board.getCellValue( board.cells[59]) + board.getCellValue( board.cells[62]))
+        cols[3] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[9]) + board.getCellValue( board.cells[12]) + board.getCellValue( board.cells[15]) + board.getCellValue( board.cells[36]) + board.getCellValue( board.cells[39]) + board.getCellValue( board.cells[42]) + board.getCellValue( board.cells[63]) + board.getCellValue( board.cells[66]) + board.getCellValue( board.cells[69]))
+        cols[4] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[10]) + board.getCellValue( board.cells[13]) + board.getCellValue( board.cells[16]) + board.getCellValue( board.cells[37]) + board.getCellValue( board.cells[40]) + board.getCellValue( board.cells[43]) + board.getCellValue( board.cells[64]) + board.getCellValue( board.cells[67]) + board.getCellValue( board.cells[70]))
+        cols[5] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[11]) + board.getCellValue( board.cells[14]) + board.getCellValue( board.cells[17]) + board.getCellValue( board.cells[38]) + board.getCellValue( board.cells[41]) + board.getCellValue( board.cells[44]) + board.getCellValue( board.cells[65]) + board.getCellValue( board.cells[68]) + board.getCellValue( board.cells[71]))
+        cols[6] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[18]) + board.getCellValue( board.cells[21]) + board.getCellValue( board.cells[24]) + board.getCellValue( board.cells[45]) + board.getCellValue( board.cells[48]) + board.getCellValue( board.cells[51]) + board.getCellValue( board.cells[72]) + board.getCellValue( board.cells[75]) + board.getCellValue( board.cells[78]))
+        cols[7] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[19]) + board.getCellValue( board.cells[22]) + board.getCellValue( board.cells[25]) + board.getCellValue( board.cells[46]) + board.getCellValue( board.cells[49]) + board.getCellValue( board.cells[52]) + board.getCellValue( board.cells[73]) + board.getCellValue( board.cells[76]) + board.getCellValue( board.cells[79]))
+        cols[8] = Helpers.getMaxDuplicateValues(board.getCellValue( board.cells[20]) + board.getCellValue( board.cells[23]) + board.getCellValue( board.cells[26]) + board.getCellValue( board.cells[47]) + board.getCellValue( board.cells[50]) + board.getCellValue( board.cells[53]) + board.getCellValue( board.cells[74]) + board.getCellValue( board.cells[77]) + board.getCellValue( board.cells[80]))
+
+        // Debug columns cell values
+        for (let i = 0; i < cols.length; i++) {
+            document.getElementById('duplicates__col-' + (i + 1)).innerHTML = cols[i]
+        }
+
+        return cols
+    }
+
+    static getMaxDuplicateValues(row) {
+        let count = {}
+        for (let i = 0; i < row.length; i++) {
+            if (count[row[i]]) {
+                count[row[i]]++
+            } else {
+                count[row[i]] = 1
+            }
+        }
+        let out = []
+        for(let c of Object.values(count)) {
+            out.push(c)
+        }
+        out.sort((a, b) => b - a)
+        return out[0]
+    }
 }
