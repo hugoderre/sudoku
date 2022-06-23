94f3f4c91c0bf6a58304dc84258908311414034d
DESKTOP-OBASNDT\Hugo <hugo.d83@outlook.fr>
2022-06-23 04:30:44 +0200

    feat: social media container is no longer injected in DOM by js but in html index refactor: sudoku sass and js DOM injection are now isolated and reusable

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,10 @@
 import Grid from "./Components/Grid";
 import GameUI from "./Components/GameUI";
 import GameController from "./Components/GameController";
-import SocialMedia from "./Components/SocialMedia";
 import './Assets/scss/style.scss';
 
 class App {
-    rootDOM: HTMLElement = document.getElementById( 'root' ) as HTMLElement
+    rootDOM: HTMLElement = document.getElementById( 'sudoku' ) as HTMLElement
     container: HTMLElement = document.createElement( 'div' )
     grid: Grid = new Grid()
     gameUI: GameUI = new GameUI( this.grid )
@@ -23,7 +22,6 @@ class App {
         this.container.append( this.gameUI.getBottomElements() )
 
         this.rootDOM.append( this.container )
-        this.rootDOM.append( new SocialMedia().getElement() )
 
         new GameController( this.grid, this.gameUI )
     }
